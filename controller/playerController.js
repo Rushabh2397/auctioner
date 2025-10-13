540ccd8263e2c5cf73caebd4628d9266b31db73f
Prajyot Sancheti <prajyot@Prajyots-Mac-mini.local>
2025-10-13 23:08:59 +0530

    changes

--- controller/playerController.js
+++ controller/playerController.js
@@ -115,32 +115,6 @@ module.exports = {
         return res.status(400).json({ message: "touranmentId is required" });
       }
 
-      // Special logic for Icon category
-      if (playerCategory === "Icon") {
-        // Check if there are any Icon players with auctionStatus = false
-        const iconPlayersCount = await players.countDocuments({
-          touranmentId: new mongoose.Types.ObjectId(touranmentId),
-          playerCategory: "Icon",
-          auctionStatus: false,
-          sold: false,
-        });
-
-        // If only 2 Icon players left (should be pushkar sancheti), fetch "pushkar sancheti"
-        if (iconPlayersCount === 2) {
-          const pushkarPlayer = await players.findOne({
-            touranmentId: new mongoose.Types.ObjectId(touranmentId),
-            name: { $regex: /pushkar sancheti/i }, // Case-insensitive search
-          });
-
-          if (pushkarPlayer) {
-            return res.status(200).json({
-              message: "Next player for auction fetched successfully",
-              data: pushkarPlayer,
-            });
-          }
-        }
-      }
-
       // Randomize selection: use aggregation with $match + $sample
       let match = {
         touranmentId: new mongoose.Types.ObjectId(touranmentId),
@@ -148,11 +122,6 @@ module.exports = {
         auctionStatus: false,
       };
       if (playerCategory) match.playerCategory = playerCategory;
-
-      // For Icon category, exclude "pushkar sancheti" if there are other Icon players available
-      if (playerCategory === "Icon") {
-        match.name = { $not: { $regex: /pushkar sancheti/i } };
-      }
 
       if (playerCategory === "Regular") {
         match = {

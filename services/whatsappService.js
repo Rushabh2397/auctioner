e4e70f57a53114516236a0bfe0afe82c92f8f8d2
Prajyot Sancheti <prajyotsancheti1772gmail.com>
2025-12-07 17:39:01 +0530

    template change

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -41,7 +41,7 @@ const sendPlayerSoldNotification = async (playerData) => {
             to: formattedMobile,
             type: "template",
             template: {
-                name: "auction_sold_message",
+                name: "sold_message",
                 language: {
                     code: "en"
                 },

5a65d04079ea93f3fcf50e8ca230f8afba903272
Prajyot Rajesh <prajyotrajesh@Prajyots-MacBook-Pro.local>
2026-04-29 21:35:00 +0530

    canges

--- utils/uploadMiddleware.js
+++ utils/uploadMiddleware.js
@@ -2,7 +2,7 @@ const { S3Client } = require('@aws-sdk/client-s3');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
